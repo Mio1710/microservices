cd2b6ecfeb0efae0836af221ae2d2a7b2828ebc1
Le Nguyen <>
2025-11-20 14:15:52 +0700

    feat(fe): update sign up

--- chat-server/my-app/src/pages/sign-up.tsx
+++ chat-server/my-app/src/pages/sign-up.tsx
@@ -14,6 +14,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Link, useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
 import * as z from 'zod';
 
 const SignUpSchema = z.object({
@@ -26,6 +27,7 @@ const SignUpSchema = z.object({
 });
 
 export const SignUp = () => {
+  const navigate = useNavigate();
   const {
     control,
     handleSubmit,
@@ -40,7 +42,7 @@ export const SignUp = () => {
   });
   const onSubmit: SubmitHandler<IRegisterDTO> = async (data: IRegisterDTO) => {
     useSignUp(data);
-    const navigate = useNavigate();
+    toast.success('Sign up successful! Please login to your account.');
     navigate('/login');
   };
 
